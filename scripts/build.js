b038e21934b53ea372d4506f951c27947e42d100
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2026-02-16 02:16:27 +0530

    fix: rename bin script to .js to fix npm publish warning

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
-import { $ } from "bun";
+import { $ } from "bun"
 
-await $`tsup src/index.ts --format esm,cjs --dts --out-dir lib`;
+await $`tsup src/index.ts --format esm,cjs --dts --out-dir lib`
 
-await $`esbuild src/cli.ts --platform=node --bundle --outfile=./bin/secenvs --format=esm --minify --external:age-encryption --banner:js="#!/usr/bin/env node"`;
+await $`esbuild src/cli.ts --platform=node --bundle --outfile=./bin/secenvs.js --format=esm --minify --external:age-encryption --banner:js="#!/usr/bin/env node"`
 
 if (process.platform !== "win32") {
-  require("fs").chmodSync("./bin/secenvs", "755");
+   require("fs").chmodSync("./bin/secenvs.js", "755")
 }

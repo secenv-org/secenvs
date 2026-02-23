3a63089864d46ca4eb07444eb23d030690abf51a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2026-02-23 12:59:28 +0530

    perf(test): migrate jest to @swc/jest to significantly reduce test suite execution times

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,24 +1,17 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-   preset: "ts-jest/presets/default-esm",
    testEnvironment: "node",
    extensionsToTreatAsEsm: [".ts"],
    moduleNameMapper: {
       "^(\\.{1,2}/.*)\\.js$": "$1",
    },
-   resolver: undefined,
    transform: {
-      "^.+\\.tsx?$": [
-         "ts-jest",
-         {
-            useESM: true,
-            tsconfig: {
-               allowJs: true,
-               module: "esnext",
-               isolatedModules: true,
-            },
+      "^.+\\.tsx?$": ["@swc/jest", {
+         jsc: {
+            target: "es2022",
+            parser: { syntax: "typescript" },
          },
-      ],
+         module: { type: "es6" }
+      }],
    },
    testTimeout: 30000,
    setupFiles: ["<rootDir>/tests/setup.js"],
